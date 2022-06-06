cdeec4d315fd13bfacdf91f2e091a799d5ddab78
esadsiz <98318801+esadsiz@users.noreply.github.com>
2022-06-06 21:48:18 +0300

    coderewiev

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-// UI vars 
+// UI vars
 
-const form = document.querySelector('form');
-const input = document.querySelector('#txtTaskName');
-const btnDeleteAll = document.querySelector('#btnDeleteAll');
-const taskList = document.querySelector('#task-list');
-const dataKey = 'items';
+const form = document.querySelector("form");
+const input = document.querySelector("#txtTaskName");
+const btnDeleteAll = document.querySelector("#btnDeleteAll");
+const taskList = document.querySelector("#task-list");
+const dataKey = "items";
 let items = [];
 
 // load items
@@ -14,112 +14,107 @@ loadItems();
 eventListeners();
 
 function eventListeners() {
-    // submit event
-    form.addEventListener('submit', addNewItem);
+  // submit event
+  form.addEventListener("submit", addNewItem);
 
-    // delete an item
-    taskList.addEventListener('click', deleteItem);
+  // delete an item
+  taskList.addEventListener("click", deleteItem);
 
-    // delete all items
-    btnDeleteAll.addEventListener('click', deleteAllItems);
+  // delete all items
+  btnDeleteAll.addEventListener("click", deleteAllItems);
 }
 
 function loadItems() {
-    items = getItemsFromLS();
-    items.forEach(function (item) {
-        createItem(item);
-    });
+  items = getItemsFromLS();
+  items.forEach(function (item) {
+    createItem(item);
+  });
 }
 
 // get items from Local Storage
 function getItemsFromLS() {
-    if (localStorage.getItem(dataKey))
-        return JSON.parse(localStorage.getItem(dataKey));
-
-    return [];
+  if (localStorage.getItem(dataKey))
+    return JSON.parse(localStorage.getItem(dataKey));
+  return [];
 }
 
 // set item to Local Storage
 function setItemToLS(text) {
-    items = getItemsFromLS();
-    items.push(text);
-    localStorage.setItem(dataKey, JSON.stringify(items));
+  items = getItemsFromLS();
+  items.push(text);
+  localStorage.setItem(dataKey, JSON.stringify(items));
 }
 
 // delete item from LS
 function deleteItemFromLS(text) {
-    items = getItemsFromLS();
-    items.forEach(function (item, index) {
-        if (item === text) {
-            items.splice(index, 1);
-        }
-    });
-    localStorage.setItem(dataKey, JSON.stringify(items));
+  items = getItemsFromLS();
+  items.forEach(function (item, index) {
+    if (item === text) {
+      items.splice(index, 1);
+    }
+  });
+  localStorage.setItem(dataKey, JSON.stringify(items));
 }
 
-
 function createItem(text) {
-    // create li
-    const li = document.createElement('li');
-    li.className = 'list-group-item list-group-item-secondary';
-    li.appendChild(document.createTextNode(text));
+  // create li
+  const li = document.createElement("li");
+  li.className = "list-group-item list-group-item-secondary";
+  li.appendChild(document.createTextNode(text));
 
-    // create a
-    const a = document.createElement('a');
-    a.classList = 'delete-item float-right';
-    a.setAttribute('href', '#');
-    a.innerHTML = '<i class="fas fa-times"></i>';
+  // create a
+  const a = document.createElement("a");
+  a.classList = "delete-item float-right";
+  a.setAttribute("href", "#");
+  a.innerHTML = '<i class="fas fa-times"></i>';
 
-    // add a to li
-    li.appendChild(a);
+  // add a to li
+  li.appendChild(a);
 
-    // add li to ul
-    taskList.appendChild(li);
-
+  // add li to ul
+  taskList.appendChild(li);
 }
 
 // add new item
 function addNewItem(e) {
-    if (!input.value) {
-        alert('Please type new task to add..');
-        return;
-    }
+  if (!input.value) {
+    alert("Please type new task to add..");
+    return;
+  }
 
-    // create item
-    createItem(input.value);
+  // create item
+  createItem(input.value);
 
-    // save to LS
-    setItemToLS(input.value);
+  // save to LS
+  setItemToLS(input.value);
 
-    // clear input
-    input.value = '';
+  // clear input
+  input.value = "";
 
-    e.preventDefault();
-
+  e.preventDefault();
 }
 
 // delete an item
 function deleteItem(e) {
-    if (e.target.className === 'fas fa-times') {
-        if (confirm('are you sure ?')) {
-            e.target.parentElement.parentElement.remove();
+  if (e.target.className === "fas fa-times") {
+    if (confirm("are you sure ?")) {
+      e.target.parentElement.parentElement.remove();
 
-            // delete item from LS
-            deleteItemFromLS(e.target.parentElement.parentElement.textContent);
-        }
+      // delete item from LS
+      deleteItemFromLS(e.target.parentElement.parentElement.textContent);
     }
-    e.preventDefault();
+  }
+  e.preventDefault();
 }
 
 // delete all items
 function deleteAllItems(e) {
-
-    if (confirm('are you sure ?')) {
-        // taskList.innerHTML='';
-        while (taskList.firstChild) {
-            taskList.removeChild(taskList.firstChild);
-        }
-        localStorage.clear();
+  if (confirm("are you sure ?")) {
+    // taskList.innerHTML='';
+    while (taskList.firstChild) {
+      taskList.removeChild(taskList.firstChild);
     }
-    e.preventDefault();
+    localStorage.clear();
+  }
+  e.preventDefault();
 }
